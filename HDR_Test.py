fad0e20994e4c69bb91f316835c076cb04406401
guoheidan <guodanda@gmail.com>
2022-09-28 20:14:26 +0800

    Initial commit

---
+++ HDR_Test.py
@@ -0,0 +1,79 @@
+import numpy as np
+import rawpy
+import os
+import sys
+import matplotlib.pyplot as plt
+from HDR_fusion import *
+from skimage import io
+from skimage import data
+import time
+import imageio
+import cv2
+
+sys.path.append(os.path.dirname(os.path.abspath(__file__)))
+# print(os.path.abspath(__file__))
+# print(os.path.dirname(os.path.abspath(__file__)))
+image_path = os.path.dirname(os.path.abspath(__file__))
+
+# raw_01_49 = rawpy.imread(os.path.join(image_path, "data\\set01\\DSC00049.ARW"))
+# raw_01_50 = rawpy.imread(os.path.join(image_path, "data\\set01\\DSC00049.ARW"))
+# raw_01_51 = rawpy.imread(os.path.join(image_path, "data\\set01\\DSC00049.ARW"))
+# raw_01_52 = rawpy.imread(os.path.join(image_path, "data\\set01\\DSC00049.ARW"))
+# raw_01_53 = rawpy.imread(os.path.join(image_path, "data\\set01\\DSC00049.ARW"))
+#
+# raw_02_113= rawpy.imread(os.path.join(image_path, "data\\set02\\DSC00113.ARW"))
+# raw_02_114 = rawpy.imread(os.path.join(image_path, "data\\set02\\DSC00114.ARW"))
+# raw_02_115 = rawpy.imread(os.path.join(image_path, "data\\set02\\DSC00115.ARW"))
+
+
+raw_03_163 = rawpy.imread(os.path.join(image_path, "data\\set03\\DSC00163.ARW"))  # 1/1000 iso 25600 f/5.6
+raw_03_164 = rawpy.imread(os.path.join(image_path, "data\\set03\\DSC00164.ARW"))  # 1/500
+raw_03_165 = rawpy.imread(os.path.join(image_path, "data\\set03\\DSC00165.ARW"))  # 1/250
+raw_03_166 = rawpy.imread(os.path.join(image_path, "data\\set03\\DSC00166.ARW"))  # 1/125
+raw_03_167 = rawpy.imread(os.path.join(image_path, "data\\set03\\DSC00167.ARW"))  # 1/60
+raw_03_168 = rawpy.imread(os.path.join(image_path, "data\\set03\\DSC00168.ARW"))  # 1/30
+raw_03_169 = rawpy.imread(os.path.join(image_path, "data\\set03\\DSC00169.ARW"))  # 1/15
+
+images = [raw_03_163, raw_03_164, raw_03_165, raw_03_166, raw_03_167, raw_03_168, raw_03_169]
+exposure_times = np.array([1 / 1000, 1 / 500, 1 / 250, 1 / 125, 1 / 60, 1 / 30, 1 / 15])
+masks = compute_mask(images)
+weights = get_fusion_weight(images)
+fuse_image = raw_exposure_fuse(images, weights, exposure_times)
+fuse_image_uint32 = fuse_image.astype(np.uint32)
+
+color_image = CFA_Interpolation(fuse_image_uint32)
+HDR_image_log_base = fastbilateral2d(color_image)
+
+color_image_uint8 = np.log2(color_image + 1).astype(np.uint8)
+plt.imshow(color_image_uint8 / np.max(color_image_uint8))
+plt.show()
+
+plt.imshow(fuse_image, cmap='gray')
+plt.show()
+
+plt.imshow(np.log(fuse_image + 1), cmap='gray')
+plt.show()
+
+image0 = images[0].raw_image
+plt.imshow(image0, cmap='gray')
+plt.show()
+image1 = images[1].raw_image
+plt.imshow(image1, cmap='gray')
+plt.show()
+image2 = images[2].raw_image
+plt.imshow(image2, cmap='gray')
+plt.show()
+image3 = images[3].raw_image
+plt.imshow(image3, cmap='gray')
+plt.show()
+image4 = images[4].raw_image
+plt.imshow(image4, cmap='gray')
+plt.show()
+image5 = images[5].raw_image
+plt.imshow(image5, cmap='gray')
+plt.show()
+
+# a = imageio.v2.imread('IMG_20220915_192911.dng')
+# imageio.imwrite('a.jpg', color_image.astype(np.uint8))
+# imageio.imwrite('test1.exr', color_image.astype(np.float64))
+#cv2.imwrite('test1.exr', color_image.astype(np.float64))
---
+++ HDR_fusion.py
@@ -0,0 +1,104 @@
+import rawpy
+import numpy as np
+import imageio
+
+
+# step 1 merge ldr ra  into 32 bit hdr image
+
+def raw_exposure_fuse(images, weights, exposure_times):
+    fuse_image = np.zeros(images[0].raw_image.shape, dtype='float64')
+    masks = compute_mask(images)
+    fuse_weights = np.zeros(weights[0].shape)
+    for i in range(0, len(images)):
+        fuse_image = fuse_image + (images[i].raw_image / exposure_times[i]) * weights[i] * masks[i]
+        fuse_weights = fuse_weights + weights[i] * masks[i]
+    eps = 10 ** -10
+    fuse_weights[fuse_weights <= 0] = eps
+    final_image = fuse_image / fuse_weights
+    return final_image
+
+
+def compute_mask(images):
+    MIN = 0.05 * 2 ** 14
+    MAX = 0.95 * 2 ** 14
+    maskList = []
+    for i in range(0, len(images)):
+        image = images[i].raw_image
+        maski = np.zeros_like(image, dtype=bool)
+        maskaa = image <= MAX
+        maskbb = image >= MIN
+        maski = maskaa & maskbb
+        maskList.append(maski)
+    return maskList
+
+
+def get_fusion_weight(images):
+    MAX = 2 ** 14
+    weights = []
+    for i in range(0, len(images)):
+        image = images[i].raw_image.copy()
+        image = image.astype('float')
+        image = image / MAX
+        image = -4 * (image - 0.5) ** 2 / (0.5 ** 2)
+        weight = np.exp(image)
+        weights.append(weight)
+    return weights
+
+
+# step 2 demosaic the fused raw data
+
+def CFA_Interpolation(fuse_image):
+    r_channel = np.zeros(fuse_image.shape, dtype='uint32')
+    gr_channel = np.zeros(fuse_image.shape, dtype=np.uint32)
+    gb_channel = np.zeros(fuse_image.shape, dtype=np.uint32)
+    b_channel = np.zeros(fuse_image.shape, dtype=np.uint32)
+    r_channel[0::2, 0::2] = fuse_image[0::2, 0::2]
+    gr_channel[0::2, 1::2] = fuse_image[0::2, 1::2]
+    gb_channel[1::2, 0::2] = fuse_image[1::2, 0::2]
+    b_channel[1::2, 1::2] = fuse_image[1::2, 1::2]
+    color_image = np.zeros((*fuse_image.shape, 3), dtype=np.uint32)
+    color_image[:, :, 0] = r_channel
+    color_image[:, :, 1] = (gr_channel + gb_channel) / 2
+    color_image[:, :, 2] = b_channel
+    return color_image
+
+
+def writeEXR(filename):
+    pass
+    return
+
+
+# step 3 tone mapping with bilateral filter
+
+def fastbilateral2d(HDR_image, space_sigma=0.02, range_sigma=0.4):
+    # image should be in float64
+    image = HDR_image.astype(np.float64)
+    log_image = np.log2(image + 1)
+    space_sigma = 0.02 * np.min(image.shape[:-1])
+    range_sigma = 0.4
+    HDR_image_log_base = log_image
+    return HDR_image_log_base
+
+
+def compute_new_intensity(HDR_image_log_base, HDR_image_log_detail, gamma):
+    pass
+    return
+
+
+# step 4 apply Gamma
+
+
+def gamma_v709(image):
+    # image should be in range [0 1 ]
+    new_image = image.copy()
+
+
+
+    return
+
+
+def v709(x):
+    if 0 <= x < 0.018:
+        return 4 * x
+    elif 0.018 <= x < 1:
+        return 1.099 * x ** 0.45 - 0.099
