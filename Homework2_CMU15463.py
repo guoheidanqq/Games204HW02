f7a4f3f531f31a63e3153f4018951e76fc7be604
guoheidan <guodanda@gmail.com>
2022-10-25 20:14:15 +0800

    gamma transform bilaterial filter

--- Homework2_CMU15463.py
+++ Homework2_CMU15463.py
@@ -70,70 +70,134 @@ from HDR_fusion import *
 # imageio.imwrite('rampgray.png', rampImg)
 
 
+
+
+# imread to images list
+images = []
+for i in range(1, 17):
+    filename = 'data/door_stack/exposure' + str(i) + '.jpg'
+    imgTmp = io.imread(filename)
+    imgTmp = imgTmp.astype(np.uint8)
+    imgTmpNormalized01 = imgTmp
+    images.append(imgTmpNormalized01)
+
+ex_times = []
+for i in range(1, 17):
+    ex_time = 2 ** (i - 1) / 2048
+    ex_times.append(ex_time)
+exposure_times = np.array(ex_times)
+
+# display images list
+for i in range(1, 17):
+    plt.subplot(4, 4, i)
+    plt.imshow(images[i - 1])
+    plt.axis('off')
+plt.show()
+
+N = 200
+images_downsampling =[]
+for i in range(0,16):
+    img = images[i]
+    img = img[::N,::N]
+    images_downsampling.append(img)
+
+for i in range(1, 17):
+    plt.subplot(4, 4, i)
+    plt.imshow(images_downsampling[i - 1])
+    plt.axis('off')
+plt.show()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 # noise calibration
-noise_images = []
-for i in range(1, 51):
-    numstr = str(i).zfill(2)
-    filename = 'data/blacknoise/' + numstr + '.tiff'
-    imgTmp = io.imread(filename)
-    if imgTmp.shape[0] > imgTmp.shape[1]:
-        imgTmp = np.transpose(imgTmp, [1, 0, 2])
-    noise_images.append(imgTmp)
-
-clip_min = 64
-clip_max = 1023
-paper_images = []
-for i in range(1, 51):
-    numstr = str(i).zfill(2)
-    filename = 'data/whitepaper/' + numstr + '.tiff'
-    imgTmp = io.imread(filename)
-    if imgTmp.shape[0] > imgTmp.shape[1]:
-        imgTmp = np.transpose(imgTmp, (1, 0, 2))
-    imgTmp
-    paper_images.append(imgTmp)
-
-black_frame = np.zeros_like(noise_images[0], np.float32)
-for i in range(0, 50):
-    black_frame = black_frame + noise_images[i]
-black_frame = black_frame / 50
-black_frame = black_frame.astype(np.uint16)
-paper_images_noise_calibration = []
-for i in range(0, 50):
-    tmp_noise_calibration = paper_images[i].astype(np.float32) - black_frame.astype(np.float32)
-    paper_images_noise_calibration.append(tmp_noise_calibration)
-
-# caculate mean image and variance image for each pixel
-images = paper_images_noise_calibration
-images_mean = np.zeros_like(images[0], dtype=np.float32)
-images_variance = np.zeros_like(images[0], dtype=np.float32)
-for i in range(0, 50):
-    images_mean = images_mean + images[i]
-
-images_mean = images_mean / 50
-
-for i in range(0, 50):
-    images_variance = images_variance + (images[i] - images_mean) ** 2
-
-images_variance = 1.0 / (50.0 - 1) * images_variance
-
-xx = images_mean.reshape(-1)
-yy = images_variance.reshape(-1)
-plt.scatter(xx,yy)
-plt.show()
-
-plt.imshow(black_frame / np.max(black_frame))
-plt.show()
-
-plt.imshow(paper_images[3] / np.max(paper_images[3]))
-plt.show()
-
-plt.imshow(paper_images_noise_calibration[3] / np.max(paper_images_noise_calibration[3]))
-plt.show()
-# plot the histogram of noise
-local_img = black_frame[::, 0]
-a = local_img.reshape(-1)
-plt.hist(a, bins=255)
-plt.show()
-
-plt.imshow(paper_images_noise_calibration[1] / 2 ** 16)
-plt.show()
+# noise_images = []
+# for i in range(1, 51):
+#     numstr = str(i).zfill(2)
+#     filename = 'data/blacknoise/' + numstr + '.tiff'
+#     imgTmp = io.imread(filename)
+#     if imgTmp.shape[0] < imgTmp.shape[1]:
+#         imgTmp = np.transpose(imgTmp, [1, 0, 2])
+#     noise_images.append(imgTmp)
+#
+# clip_min = 64
+# clip_max = 1023
+# paper_images = []
+# for i in range(1, 51):
+#     numstr = str(i).zfill(2)
+#     filename = 'data/whitepaper/' + numstr + '.tiff'
+#     imgTmp = io.imread(filename)
+#     if imgTmp.shape[0] < imgTmp.shape[1]:
+#         imgTmp = np.transpose(imgTmp, (1, 0, 2))
+#     imgTmp
+#     paper_images.append(imgTmp)
+#
+# black_frame = np.zeros_like(noise_images[0], np.float32)
+# for i in range(0, 50):
+#     black_frame = black_frame + noise_images[i]
+# black_frame = black_frame / 50
+# black_frame = black_frame.astype(np.uint16)
+# paper_images_noise_calibration = []
+# for i in range(0, 50):
+#     tmp_noise_calibration = paper_images[i].astype(np.float32) - black_frame.astype(np.float32)
+#     paper_images_noise_calibration.append(tmp_noise_calibration)
+#
+# # caculate mean image and variance image for each pixel
+# images = paper_images_noise_calibration
+# images_mean = np.zeros_like(images[0], dtype=np.float32)
+# images_variance = np.zeros_like(images[0], dtype=np.float32)
+# for i in range(0, 50):
+#     images_mean = images_mean + images[i]
+#
+# images_mean = images_mean / 50
+#
+# for i in range(0, 50):
+#     images_variance = images_variance + (images[i] - images_mean) ** 2
+#
+# images_variance = 1.0 / (50.0 - 1) * images_variance
+#
+# xx = images_mean.reshape(-1)
+# yy = images_variance.reshape(-1)
+# a,b = np.polyfit(xx,yy,1)
+# plt.scatter(xx, yy)
+# plt.scatter(xx,a*xx + b)
+# plt.show()
+#
+# #line fit
+#
+#
+#
+# plt.imshow(black_frame / np.max(black_frame))
+# plt.show()
+#
+# plt.imshow(paper_images[3] / np.max(paper_images[3]))
+# plt.show()
+#
+# plt.imshow(paper_images_noise_calibration[3] / np.max(paper_images_noise_calibration[3]))
+# plt.show()
+# # plot the histogram of noise
+# local_img = black_frame[::, 0]
+# a = local_img.reshape(-1)
+# plt.hist(a, bins=255)
+# plt.show()
+#
+# plt.imshow(paper_images_noise_calibration[1] / 2 ** 16)
+# plt.show()
+#
+# #show mean image
+# plt.imshow(np.sqrt(images_mean/np.max(images_mean)))
+# plt.show()
+# #show variance image
+# plt.imshow(np.sqrt(images_variance/np.max(images_variance)))
+# plt.show()
